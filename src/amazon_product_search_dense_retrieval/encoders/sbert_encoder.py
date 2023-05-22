0539a84dcf7084eb5dcf667c7d6333c9ca41dca8
rejasupotaro <takiguchi0817@gmail.com>
2023-05-23 07:28:08 +0900

    Define PoolingMode

--- src/amazon_product_search_dense_retrieval/encoders/sbert_encoder.py
+++ src/amazon_product_search_dense_retrieval/encoders/sbert_encoder.py
@@ -4,9 +4,11 @@ from sentence_transformers import SentenceTransformer
 from sentence_transformers.models import Pooling, Transformer
 from torch import Tensor
 
+PoolingMode = Literal["cls", "max", "mean"]
+
 
 class SBERTEncoder:
-    def __init__(self, bert_model_name: str, pooling_mode: Literal["cls", "max", "mean"] = "mean"):
+    def __init__(self, bert_model_name: str, pooling_mode: PoolingMode = "mean"):
         transformer = Transformer(bert_model_name)
         pooling = Pooling(
             word_embedding_dimension=transformer.get_word_embedding_dimension(),
