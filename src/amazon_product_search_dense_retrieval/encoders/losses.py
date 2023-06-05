1bd4d903633804f8f1a9c8e15c370256790aaaa8
rejasupotaro <takiguchi0817@gmail.com>
2023-06-05 09:41:15 +0900

    Normalize vectors

--- src/amazon_product_search_dense_retrieval/encoders/losses.py
+++ src/amazon_product_search_dense_retrieval/encoders/losses.py
@@ -1,6 +1,7 @@
 import torch
 from torch import Tensor
-from torch.nn import Module, functional
+from torch.nn import Module
+from torch.nn.functional import cosine_similarity, log_softmax, normalize
 
 
 class TripletLoss(Module):
@@ -9,8 +10,8 @@ class TripletLoss(Module):
         self.margin = margin
 
     def forward(self, query: Tensor, pos_doc: Tensor, neg_doc: Tensor) -> Tensor:
-        pos_logit = functional.cosine_similarity(query, pos_doc)
-        neg_logit = functional.cosine_similarity(query, neg_doc)
+        pos_logit = cosine_similarity(query, pos_doc)
+        neg_logit = cosine_similarity(query, neg_doc)
         loss = torch.clamp(neg_logit - pos_logit + self.margin, min=0)
         return loss
 
@@ -20,9 +21,19 @@ class InfoNCELoss(Module):
         super().__init__()
 
     def forward(self, query: Tensor, pos_doc: Tensor, neg_doc: Tensor) -> Tensor:
-        pos_logit = (query * pos_doc).sum(-1).unsqueeze(1)
-        neg_logit = torch.matmul(query, neg_doc.transpose(0, 1))
-        logit = torch.cat([pos_logit, neg_logit], dim=1)
-        lsm = functional.log_softmax(logit, dim=1)
+        # Normalize the given vectors before computing the doc product.
+        query = normalize(query, dim=-1)
+        pos_doc = normalize(pos_doc, dim=-1)
+        neg_doc = normalize(neg_doc, dim=-1)
+
+        # Compute the dot product of
+        # 1. query (batch_size, vec_len) and pos_doc (batch_size, vec_len) => (batch_size) => (batch_size, 1)
+        pos_logit = (query * pos_doc).sum(-1).unsqueeze(-1)
+        # 1. query (batch_size, vec_len) and neg_docs (batch_size, vec_len).T => (batch_size, batch_size)
+        neg_logit = torch.matmul(query, neg_doc.t())
+
+        # Concat pos_logit and neg_logits => (batch_size, batch_size + 1)
+        logit = torch.cat([pos_logit, neg_logit], dim=-1)
+        lsm = log_softmax(logit, dim=-1)
         loss = -1.0 * lsm[:, 0]
         return loss
--- tests/encoders/test_losses.py
+++ tests/encoders/test_losses.py
@@ -1,23 +1,39 @@
 import torch
 
-from amazon_product_search_dense_retrieval.encoders.losses import TripletLoss
+from amazon_product_search_dense_retrieval.encoders.losses import (
+    InfoNCELoss,
+    TripletLoss,
+)
 
 
 def test_triplet_loss():
     vec = torch.tensor([[1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0]])
+    criteria = TripletLoss()
 
     query = vec
     pos_doc = vec
     neg_doc = -vec
-
-    criteria = TripletLoss()
     loss = criteria.forward(query, pos_doc, neg_doc)
     assert loss.eq(torch.tensor([0.0, 0.0])).all()
 
     query = vec
     pos_doc = -vec
     neg_doc = vec
-
-    criteria = TripletLoss()
     loss = criteria.forward(query, pos_doc, neg_doc)
     assert loss.eq(torch.tensor([2.4, 2.4])).all()
+
+
+def test_info_nce_loss():
+    vec = torch.tensor([[1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0]])
+    criteria = InfoNCELoss()
+
+    query = vec
+    pos_doc = vec
+    neg_doc = -vec
+    low_loss = criteria.forward(query, pos_doc, neg_doc).mean()
+
+    query = vec
+    pos_doc = -vec
+    neg_doc = vec
+    high_loss = criteria.forward(query, pos_doc, neg_doc).mean()
+    assert low_loss < high_loss
