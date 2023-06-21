774d2e025243adfcb9667c25a6fa6f17d8631ff0
rejasupotaro <takiguchi0817@gmail.com>
2023-06-22 06:59:05 +0900

    Delete Encoder protocol

--- src/amazon_product_search_dense_retrieval/encoders/__init__.py
+++ src/amazon_product_search_dense_retrieval/encoders/__init__.py
@@ -1,11 +1,9 @@
 from .bert_encoder import BERTEncoder
 from .bi_bert_encoder import BiBERTEncoder
-from .encoder import Encoder
 from .sbert_encoder import SBERTEncoder
 
 __all__ = [
     "BERTEncoder",
     "BiBERTEncoder",
-    "Encoder",
     "SBERTEncoder",
 ]
