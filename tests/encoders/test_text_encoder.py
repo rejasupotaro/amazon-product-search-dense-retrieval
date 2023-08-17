76751c2a3516c1be6cf37d342233273061f9aae0
rejasupotaro <takiguchi0817@gmail.com>
2023-08-17 16:02:10 +0900

    Fix tests

--- tests/encoders/test_text_encoder.py
+++ tests/encoders/test_text_encoder.py
@@ -10,8 +10,8 @@ from amazon_product_search_dense_retrieval.encoders.text_encoder import (
 @pytest.mark.parametrize(
     ("texts", "expected"),
     [
-        ("text", (128,)),
-        (["text"], (768, 128), (1, 128)),
+        ("text", (768,)),
+        (["text"], (1, 768)),
     ],
 )
 def test_encode(texts, expected):
@@ -23,17 +23,6 @@ def test_encode(texts, expected):
 
     text_embs = encoder.encode(texts)
     assert text_embs.shape == expected
-
-
-def test_encode_many_texts():
-    encoder = TextEncoder(
-        hf_model_name="sonoisa/sentence-luke-japanese-base-lite",
-        hf_model_trainable=False,
-        pooling_mode="cls",
-    )
-
-    text_embs = encoder.encode(["text1", "text2", "text3"], batch_size=2)
-    assert text_embs.shape == (3, 768)
 
 
 def test_save_and_load(tmp_path):
@@ -51,10 +40,7 @@ def test_save_and_load(tmp_path):
     )
     model_name = "text_encoder"
     model_filepath = save(models_dir, model_name, encoder)
-    assert (
-        model_filepath.split("/")[-1]
-        == "sonoisa_sentence-luke-japanese-base-lite_mean_query_768_768.pt"
-    )
+    assert model_filepath.split("/")[-1] == "text_encoder.pt"
 
     encoder = load(
         models_dir, model_name, hf_model_name, hf_model_trainable, pooling_mode
