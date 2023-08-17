8e02fe5c83d18026ddd42b7135bf295edc6a386c
rejasupotaro <takiguchi0817@gmail.com>
2023-08-17 13:15:17 +0900

    Implement TextEncoder

---
+++ src/amazon_product_search_dense_retrieval/encoders/text_encoder.py
@@ -0,0 +1,96 @@
+import numpy as np
+import torch
+from more_itertools import chunked
+from torch import Tensor
+from torch.nn import Module
+from transformers import AutoModel, AutoTokenizer
+
+from amazon_product_search_dense_retrieval.encoders.modules.pooler import (
+    Pooler,
+    PoolingMode,
+)
+
+
+class TextEncoder(Module):
+    def __init__(
+        self,
+        hf_model_name: str,
+        hf_model_trainable: bool,
+        pooling_mode: PoolingMode,
+    ) -> None:
+        super().__init__()
+        self.hf_model_name = hf_model_name
+        self.tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
+        self.bert_model = AutoModel.from_pretrained(hf_model_name)
+        for param in self.bert_model.parameters():
+            param.requires_grad = hf_model_trainable
+        self.pooler = Pooler(pooling_mode)
+
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.to(self.device)
+
+    @staticmethod
+    def build_model_name(
+        bert_model_name: str,
+        pooling_mode: PoolingMode,
+    ):
+        bert_model_name = bert_model_name.replace("/", "_")
+        return f"{bert_model_name}_{pooling_mode}"
+
+    def save(self, models_dir: str) -> str:
+        model_name = self.build_model_name(self.hf_model_name, self.pooler.pooling_mode)
+        model_filepath = f"{models_dir}/{model_name}.pt"
+        torch.save(self.projection.state_dict(), model_filepath)
+        return model_filepath
+
+    @staticmethod
+    def load(
+        bert_model_name: str,
+        bert_model_trainable: bool,
+        pooling_mode: PoolingMode,
+        models_dir: str,
+    ) -> "TextEncoder":
+        encoder = TextEncoder(
+            bert_model_name,
+            bert_model_trainable,
+            pooling_mode,
+        )
+        model_name = TextEncoder.build_model_name(bert_model_name, pooling_mode)
+        model_filepath = f"{models_dir}/{model_name}.pt"
+        encoder.projection.load_state_dict(torch.load(model_filepath))
+        return encoder
+
+    def tokenize(self, texts) -> dict[str, Tensor]:
+        tokens = self.tokenizer(
+            texts,
+            add_special_tokens=True,
+            padding="longest",
+            truncation="longest_first",
+            return_attention_mask=True,
+            return_tensors="pt",
+        )
+        return tokens
+
+    def forward(self, tokens: dict[str, Tensor]) -> Tensor:
+        token_embs = self.bert_model(**tokens).last_hidden_state
+        text_emb = self.pooler.forward(token_embs, tokens["attention_mask"])
+        return text_emb
+
+    def encode(self, texts: str | list[str], batch_size: int = 32) -> np.ndarray:
+        input_was_string = False
+        if isinstance(texts, str):
+            input_was_string = True
+            texts = texts
+
+        self.eval()
+        all_embs: list[np.ndarray] = []
+        with torch.no_grad():
+            for batch in chunked(texts, n=batch_size):
+                tokens = self.tokenize(batch)
+                for key in tokens:
+                    if isinstance(tokens[key], Tensor):
+                        tokens[key] = tokens[key].to(self.device)
+                embs: Tensor = self(tokens)
+                embs = embs.detach().cpu().numpy()
+                all_embs.extend(embs)
+        return np.array(all_embs)[0] if input_was_string else np.array(all_embs)
